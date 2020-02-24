90224bc199bd9f401edf2fb91fb5f5388ffa7fa3
e <2424932515@qq.com>
2020-02-25 00:29:37 +0800

    feat；修改邮件注册方式

--- ele-admin-ts/app/controller/user.ts
+++ ele-admin-ts/app/controller/user.ts
@@ -4,7 +4,7 @@
  * @Author: 笑佛弥勒
  * @Date: 2020-02-18 17:21:14
  * @LastEditors: 笑佛弥勒
- * @LastEditTime: 2020-02-18 23:47:22
+ * @LastEditTime: 2020-02-25 00:20:01
  */
 
 import { BaseController } from "../core/baseController"
@@ -25,11 +25,16 @@ export default class User extends BaseController {
       return
     }
     try {
-      let data = await this.ctx.helper.sendEmail(email, this.app)
-      if (data.hasExit) {
+      // let data = await this.ctx.helper.sendEmail(email, this.app) // 发送邮箱
+      
+      const codeExit = await this.app.redis.get(`code_${email}`)
+      if (codeExit) {
         this.success(200, '不可重复发送')
+        return
       }
-      this.success(200, '发送成功')
+      const code = this.ctx.helper.random(10000,99999)
+      await this.app.redis.set(`code_${email}`, code, 'ex', 3600)
+      this.success(200, '发送成功', code)
     } catch (error) {
       this.fail(500, '发送失败')
     }
@@ -63,6 +68,7 @@ export default class User extends BaseController {
           email: email,
           user_name: email
         })
+        let userDetail:any = await this.ctx.service.user.getUserByEmail(email)
         await this.ctx.helper.loginToken({ user_id: userDetail['user_id'], email: userDetail['email'] }).then((res) => token = res) // 取到生成token
         await this.app.redis.set(email, token, 'ex', 7200) // 保存到redis
         this.ctx.cookies.set('authorization', token, {
