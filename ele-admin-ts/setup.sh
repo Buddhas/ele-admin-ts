9c0ca9810b9d5b79a34ce5523866985be487a499
chenbin <2424932515@qq.com>
2020-03-23 15:09:02 +0800

    feat:修改镜像名称

--- ele-admin-ts/setup.sh
+++ ele-admin-ts/setup.sh
@@ -5,13 +5,13 @@ docker stop ele_admin_ts || true;
 # 删除ele_admin_ts容器
 docker rm ele_admin_ts || true;
 # 删除ele/index/vue镜像
-docker rmi --force $(docker images | grep ele/index/vue | awk '{print $3}')
+docker rmi --force $(docker images | grep ele/admin/ts | awk '{print $3}')
 # 构建ele/index/vue:$image_version镜像
-docker build . -t ele/index/vue:$image_version;
+docker build . -t ele/admin/ts:$image_version;
 # 查看镜像列表
 docker images;
 # 基于ele/index/vue 镜像 构建一个容器 ele_admin_ts
-docker run -p 9528:7001 -d --name ele_admin_ts ele/index/vue:$image_version;
+docker run -p 9528:7001 -d --name ele_admin_ts ele/admin/ts:$image_version;
 # 查看日志
 docker logs ele_admin_ts;
 #删除build过程中产生的镜像    #docker image prune -a -f
